a2eb2066300577936bc14e97e38a4c8e7f6d2d96
nick repasy <nrepasy@gmail.com>
2020-11-26 03:32:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,8 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  famous_bands = ["Smiths", "Beatles"]
-  "Beatles" = famous_bands.pop
+  famous_bands = ["Smiths", "Beatles" , "arrays!"]
+  "arrays!" = famous_bands.pop
 end
 
 def remove_element_from_start_of_array(array)
