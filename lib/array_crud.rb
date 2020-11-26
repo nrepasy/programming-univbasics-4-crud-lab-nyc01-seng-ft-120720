7351653c63c2779d24e3d595e11af11c34815e44
nick repasy <nrepasy@gmail.com>
2020-11-26 03:45:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  famous_bands = ["Smiths", "Beatles" , "am"  ]
+  famous_band = ["Smiths", "Beatles" , "am"  ]
   famous_band[2]
 end
 
