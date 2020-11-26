3b2d3e699ce93a05b2aab6b494c14d7155e91eb7
nick repasy <nrepasy@gmail.com>
2020-11-26 03:29:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  famous_bands.unshift
+  famous_bands.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
