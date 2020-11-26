b76c85062e5798b098158f6af1802bbd0314dfe8
nick repasy <nrepasy@gmail.com>
2020-11-26 03:42:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   famous_bands = ["Smiths", "Beatles" , "arrays!"]
-  "arrays!" = famous_bands.pop
+  arrays = famous_bands.pop
 end
 
 def remove_element_from_start_of_array(array)
