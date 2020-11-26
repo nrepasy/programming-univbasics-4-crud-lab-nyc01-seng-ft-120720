506d3eaf5050d3fe7774a3f852960f467e8ccda8
nick repasy <nrepasy@gmail.com>
2020-11-26 03:47:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-    famous_band = ["wow", "wee"]
+    famous_band = ["wee", "wow"]
     famous_band[-1]
 end
 
