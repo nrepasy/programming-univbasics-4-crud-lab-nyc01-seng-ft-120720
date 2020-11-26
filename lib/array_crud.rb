4743ea0c6581c34e6f6047689e428c33fdb84af4
nick repasy <nrepasy@gmail.com>
2020-11-26 03:43:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  famous_band = ["wow", "wee"]
 end
 
 def retrieve_element_from_index(array, index_number)
