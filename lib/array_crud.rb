459ed43b4768334645a4aec01c0f24c2a75e56c2
nick repasy <nrepasy@gmail.com>
2020-11-26 03:47:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -41,7 +41,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  famous_bands = ["Smiths", "Beatles" , "arrays!"]
+  famous_bands[-1]
 end
 
 def update_element_from_index(array, index_number, element)
